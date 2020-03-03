04ff8848ec771b07d342d50de22affbcc3d09a39
1026minjae <minjae2k1@gmail.com>
2020-03-03 19:39:10 +0900

    1463번 품

--- 1463.py
+++ 1463.py
@@ -1,8 +1,13 @@
-x = input()
+x = int(input())
 
-if !(x % 3) : 
-    x /= 3
-elif !(x%2) : 
-    x /= 2
-else : 
-    x -= 1
+cnt = [0 for _ in range(x+1)]
+
+for i in range(x+1) :
+    if not i : continue
+    temp = cnt[i] + 1
+    if i+1 <= x and (not cnt[i+1] or cnt[i+1] > temp) : cnt[i+1] = temp
+    if i*2 <= x and (not cnt[i*2] or cnt[i*2] > temp) : cnt[i*2] = temp
+    if i*3 <= x and (not cnt[i*3] or cnt[i*3] > temp) : cnt[i*3] = temp
+
+print(cnt[x])
+    

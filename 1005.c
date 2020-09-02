7510ed242883532b27e801eedc7ec6655041a305
1026minjae <minjae2k1@gmail.com>
2020-09-02 23:47:27 +0900

    우선 다 짬. 그러나 틀림. 가장 하위의 것부터 연산하도록 고쳐야함.

---
+++ 1005.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int ANS[1000];
+    int T, N, K, fro, hind, goal;
+    int i,j;
+    int *D;
+    int *ans;
+
+    scanf("%d", &T);
+
+    for(i=0;i<T;i++) {
+        scanf("%d %d",&N,&K);
+        D = malloc(sizeof(int) * N); //int가 4바이트일 것 같긴한데 컴퓨터마다 다를 수도 있다고 들음
+        ans = malloc(sizeof(int) * N);
+
+        for(j=0;j<N;j++) {
+            scanf("%d",&D[j]);
+            ans[j]=D[j];
+        }
+
+        for(j=0;j<K;j++) {
+            scanf("%d %d",&fro,&hind);
+            if(ans[fro-1]+D[hind-1] > ans[hind-1])
+                ans[hind-1] = ans[fro-1]+D[hind-1];
+        }
+
+        scanf("%d",&goal);
+        ANS[i]=ans[goal-1];
+
+        free(D);
+        free(ans);
+    }
+
+    for(i=0;i<T;i++)
+        printf("%d\n",ANS[i]);
+    return 0;
+}

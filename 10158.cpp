da5d90d06719abd3caaa824bc685a51b6e6f1aa5
1026minjae <minjae2k1@gmail.com>
2021-02-06 13:35:20 +0900

    2021 2 6

---
+++ 10158.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int main() {
+    int w, h, p, q, t, x, y;
+
+    scanf("%d %d\n%d %d\n%d",&w,&h,&p,&q,&t);
+
+    if(((p+t)/w) % 2)
+        x = w - ((p+t) % w);
+    else
+        x = (p+t) % w;
+    if(((q+t)/h) % 2)
+        y = h - ((q+t) % h);
+    else
+        y = (q+t) % h;
+
+    printf("%d %d",x,y);
+
+    return 0;
+}

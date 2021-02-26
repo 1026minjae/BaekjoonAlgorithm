68206fd8f5aa4f48bc62fa0323a5a1840ebff1ef
1026minjae <minjae2k1@gmail.com>
2021-02-26 16:00:10 +0900

    2021 2 26

---
+++ 1029.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main() {
+    int N, i;
+    char costs[15][15];
+
+    scanf("%d",&N);
+    for(i=0;i<N;++i)
+        scanf("%s",costs[i]);
+
+    /*
+
+    구체적인 알고리즘 구현
+
+    */
+    
+    return 0;
+}

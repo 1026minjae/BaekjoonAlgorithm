caf9241b49a6327ede54c2360226bc3eab519aaf
1026minjae <minjae2k1@gmail.com>
2021-02-28 22:30:25 +0900

    2021 2 28

--- 1074.cpp
+++ 1074.cpp
@@ -1,10 +1,34 @@
 #include <cstdio>
 
-/*
-위치를 판정한 뒤 그 위치를 기반으로 답을 도출하면 됨
-*/
+int powerof2[16] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768};
+
+int number(int n, int r, int c) {
+    if(n==0) return 0;
+
+    if(r < powerof2[n-1]) {
+        if(c < powerof2[n-1]) { // first
+            return number(n-1,r,c);
+        }
+        else { // second 
+            return powerof2[n-1]*powerof2[n-1] + number(n-1,r,c-powerof2[n-1]);
+        }
+    }
+    else {
+        if(c < powerof2[n-1]) { // third
+            return 2 * powerof2[n-1]*powerof2[n-1] + number(n-1,r-powerof2[n-1],c);
+        }
+        else { // fourth
+            return 3 * powerof2[n-1]*powerof2[n-1] + number(n-1,r-powerof2[n-1],c-powerof2[n-1]);
+        }
+    }
+}
 
 int main() {
+    int N, r, c;
+
+    scanf("%d %d %d",&N,&r,&c);
+
+    printf("%d",number(N,r,c));
     
     return 0;
 }
